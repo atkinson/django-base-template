2793d4e7a98b1bbc5dbc26b2c57c8d8698243d39
Rich <rich@piran.com.au>
2012-03-24 00:35:37 +1100

    cool user menu

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.defaults import include, url, patterns
-from session_csrf import anonymous_csrf
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,7 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin_tools/', include('admin_tools.urls')),
     (r'', include('{{ project_name }}.base.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/$', anonymous_csrf(admin.site.admin_view(admin.site.index))),
     (r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('userena.urls')),
     #url(r'^', include('debug_toolbar_user_panel.urls')),
